9062d130d5621263c2aaf925a5e44745f403a2bf
Squallium <squallium3@gmail.com>
2015-01-02 18:55:24 +0100

    Generated apk with maven, no proguard

--- install-jars.sh
+++ install-jars.sh
@@ -1,1 +1,2 @@
 mvn install:install-file -Dfile=in-app-purchasing-1.0.3.jar -DgroupId=com.amazon.inapp.purchasing -DartifactId=in-app-purchasing -Dpackaging=jar -Dversion=1.0.3 -DgeneratePom=true
+mvn install:install-file -Dfile=pom.xml -DgroupId=com.squallium.androidcommons.inapp -DartifactId=Android-Commos-InApp -Dpackaging=pom -Dversion=1.0.1 -DgeneratePom=true
